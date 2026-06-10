dc8ecd318b5a9d87772bb557c35becf4c43a497a
krawtschenko <eugenykravchenko@gmail.com>
2026-06-10 22:01:09 +0200

    feat: add Chip component with Toggle Group from Radix

---
+++ src/components/ui/chip/chip.tsx
@@ -0,0 +1,23 @@
+import * as ToggleGroupPrimitive from '@radix-ui/react-toggle-group'
+import clsx from 'clsx'
+import type { ComponentPropsWithoutRef } from 'react'
+
+import s from './chip.module.scss'
+
+export const Chip = ({
+  className,
+  ...rest
+}: ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Root>) => {
+  return (
+    <ToggleGroupPrimitive.Root className={clsx(s.group, className)} {...rest} />
+  )
+}
+
+export const ChipItem = ({
+  className,
+  ...rest
+}: ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item>) => {
+  return (
+    <ToggleGroupPrimitive.Item className={clsx(s.item, className)} {...rest} />
+  )
+}
---
+++ src/components/ui/chip/index.ts
@@ -0,0 +1,1 @@
+export { Chip, ChipItem } from './chip'
